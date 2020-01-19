682cd003044188270843a01fc0b3e640dfdb7d2a
Gabriel Guimarães Vilas Boas Marin <gabrielvbmarin21@gmail.com>
2020-01-19 18:27:37 -0300

    heroku setup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 // define paths | express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -87,6 +88,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server up in 3000")
+app.listen(port, () => {
+    console.log("Server up in " + port)
 })
